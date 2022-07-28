1762b5571e9064e78544534d89dc8257b6c6a1d2
Martin Sakac <msakac@foi.hr>
2022-07-28 10:53:28 +0200

    Added api resource that stores data in mongodb database

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,13 +1,17 @@
-import NewMeetupForm from '../../components/meetups/NewMeetupForm'
-import React from 'react'
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import React from "react";
 
 export default function NewMeetup() {
+  async function addMeetupHandler(enteredMeetupData) {
+    const resonse = await fetch("/api/new-meetup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(enteredMeetupData)
+    });
 
-    function addMeetupHandler(enteredMeetupData){
-        console.log(enteredMeetupData);
-    }
+    const data = await resonse.json();
+    console.log(data.message);
+  }
 
-  return (
-    <NewMeetupForm onAddMeetup={addMeetupHandler}/>
-  )
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 }
